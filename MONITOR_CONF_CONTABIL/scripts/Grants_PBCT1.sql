766a7a143a092d47e000946b7862829e4d5c70be
A0070047 <A0070047@NPRCURJB3264017.gvt.net.br>
2018-04-17 10:00:50 -0300

    GRANTS

--- MONITOR_CONF_CONTABIL/scripts/Grants_PBCT1.sql
+++ MONITOR_CONF_CONTABIL/scripts/Grants_PBCT1.sql
@@ -9,6 +9,35 @@ grant select,insert,update,delete on arbor.jnl_keys to PROATIVO_PROD;
 grant select,insert,update,delete on arbor.jnl_keys to SAN_AUTOM;
 grant select on arbor.jnl_keys  to R_ITBILLING_RO;
 
+grant select,insert,update,delete on arbor.jnl_detail to PROATIVO_PROD;
+grant select,insert,update,delete on arbor.jnl_detail to SAN_AUTOM;
+grant select on arbor.jnl_detail to R_ITBILLING_RO;
+
 grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_TAXAS_IMPOSTOS to PROATIVO_PROD;
 grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_TAXAS_IMPOSTOS to SAN_AUTOM;
 grant select on ARBORGVT_JOURNALS.GVT_TAXAS_IMPOSTOS  to R_ITBILLING_RO;
+
+
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_DIVISAO to PROATIVO_PROD;
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_DIVISAO to SAN_AUTOM;
+grant select on ARBORGVT_JOURNALS.GVT_KENAN_SAP_DIVISAO to R_ITBILLING_RO;
+
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_ORD_INT to PROATIVO_PROD;
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_ORD_INT to SAN_AUTOM;
+grant select on ARBORGVT_JOURNALS.GVT_KENAN_SAP_ORD_INT  to R_ITBILLING_RO;
+
+grant select,insert,update,delete on arborgvt_journals.GVT_KENAN_SAP_SPED_CONT_DET to PROATIVO_PROD;
+grant select,insert,update,delete on arborgvt_journals.GVT_KENAN_SAP_SPED_CONT_DET to SAN_AUTOM;
+grant select on arborgvt_journals.GVT_KENAN_SAP_SPED_CONT_DET to R_ITBILLING_RO;
+
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_CENTRO_CUSTO to PROATIVO_PROD;
+grant select,insert,update,delete on ARBORGVT_JOURNALS.GVT_KENAN_SAP_CENTRO_CUSTO to SAN_AUTOM;
+grant select on ARBORGVT_JOURNALS.GVT_KENAN_SAP_CENTRO_CUSTO to R_ITBILLING_RO;
+
+grant select,insert,update,delete on arborgvt_billing.Gvt_Classificacao_Tv to PROATIVO_PROD;
+grant select,insert,update,delete on arborgvt_billing.Gvt_Classificacao_Tv to SAN_AUTOM;
+grant select on arborgvt_billing.Gvt_Classificacao_Tv to R_ITBILLING_RO;
+
+grant select,insert,update,delete on ARBORGVT_JOURNALS.Gvt_Kenan_Sap_Sped_Lotes to PROATIVO_PROD;
+grant select,insert,update,delete on ARBORGVT_JOURNALS.Gvt_Kenan_Sap_Sped_Lotes to SAN_AUTOM;
+grant select on ARBORGVT_JOURNALS.Gvt_Kenan_Sap_Sped_Lotes to R_ITBILLING_RO;
